4520927d76a6a831be0fe47dfe135a368f7f8e03
Chandu2828 <purnachand7569@gmail.com>
2026-06-24 23:54:41 +0530

    added files

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -5,24 +5,24 @@ resource "aws_ssm_parameter" "vpc_id" {
     overwrite   = true 
 }
 
-# resource "aws_ssm_parameter" "public_subnet_ids" {
-#     name = "/${var.project}/${var.environment}/public_subnet_ids"
-#     type = "String"
-#     value = join(",", module.vpc.public_subnet_ids) # because the value is list(tuple) but it expects as a string 
-#     overwrite = true 
-# }
+resource "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.public_subnet_ids) # because the value is list(tuple) but it expects as a string 
+    overwrite = true 
+}
 
-# resource "aws_ssm_parameter" "private_subnet_ids" {
-#   name  = "/${var.project}/${var.environment}/private_subnet_ids"
-#   type  = "String"
-#   value = join(",", module.vpc.private_subnet_ids)
-#   overwrite = true
-# }
+resource "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.private_subnet_ids) # because the value is list(tuple) but it expects as a string 
+    overwrite = true 
+}
 
-# resource "aws_ssm_parameter" "database_subnet_ids" {
-#   name  = "/${var.project}/${var.environment}/database_subnet_ids"
-#   type  = "String"
-#   value = join(",", module.vpc.database_subnet_ids)
-#   overwrite = true
-# }
+resource "aws_ssm_parameter" "database_subnet_ids" {
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
+    type = "String"
+    value = join(",", module.vpc.database_subnet_ids) # because the value is list(tuple) but it expects as a string 
+    overwrite = true 
+}
 
