d06aec9bd22916cb368c4bb9aa64f24eeb461280
Chandu2828 <purnachand7569@gmail.com>
2026-06-29 19:12:59 +0530

    modified files

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -120,7 +120,7 @@ resource "aws_instance" "mysql" {
     ami = data.aws_ami.joindevops.id 
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mysql_sg_id]
-    subnet_id = local.database_subnet_id 
+    subnet_id = local.database_subent_id
     iam_instance_profile = aws_iam_instance_profile.mysql.name 
 
     tags = merge (
