65bf9fb3a573646cc3aec9c655db6475bb55e0b4
Chandu2828 <purnachand7569@gmail.com>
2026-06-29 12:38:48 +0530

    modified files

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "redis" {
     ami                     = data.aws_ami.joindevops.id 
     instance_type           = "t3.micro"
     vpc_security_group_ids  = [local.redis_sg_id]
-    subnet_id               = local.database_subnet.id
+    subnet_id               = local.database_subnet_id
 
     tags = merege(
         {
@@ -82,7 +82,7 @@ resource "aws_instance" "rabbitmq" {
     ami = data.aws_ami.joindevops.id 
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.rabbitmq_sg_id]
-    subnet_id = local.database_subnet.id
+    subnet_id = local.database_subnet_id
 
     tags = merge(
         {
