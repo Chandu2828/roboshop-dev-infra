c29cc133da4ca10975dc6bd8f4eb0e1d6b46d7d4
Chandu2828 <purnachand7569@gmail.com>
2026-06-29 19:18:08 +0530

    modified files

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -11,7 +11,7 @@ data "aws_ssm_parameter" "rabbitmq_sg_id"{
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-    name = "${var.project}/${var.environment}/mysql_sg_id"
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
