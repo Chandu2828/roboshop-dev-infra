a9fbde2c2e368519a38e4a0ac95014886bdcf35d
Chandu2828 <purnachand7569@gmail.com>
2026-07-01 23:36:31 +0530

    modified files

--- 70-acm/variables.tf
+++ 70-acm/variables.tf
@@ -6,10 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
-variable "zone_id" {
-    default = "Z007238534KR4DH682ASN"
-}
-
 variable "domain_name" {
     default = "vnmurthy.online"
+}
+
+variable "zone_id" {
+    default = "Z007238534KR4DH682ASN"
 }
